0c437f75470b0de477a791b60f890fe150de2243
Titvt <tqydyqt@126.com>
2025-11-17 11:53:16 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-dnf install -y git python3-pip
 firewall-cmd --permanent --add-service=http
 firewall-cmd --reload
-python app.py
+dnf install -y git python3-pip screen
+pip install gunicorn
+chmod 777 /run/screen
+screen -S app
+gunicorn -w 3 -b 0.0.0.0:80 app:app
